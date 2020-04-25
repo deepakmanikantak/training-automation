8f13e5caf6dff0025fa0a8797da34cbaf16c6d30
VamshiTachyon <vamshikrishna.m@tachyontech.com>
2020-04-25 09:41:34 -0700

    ArraySum Program

---
+++ src/com/trainee/vamsi/ArraySum.java
@@ -0,0 +1,26 @@
+package com.trainee.vamsi;
+import java.util.Scanner;
+
+
+public class ArraySum
+{
+    public static void main(String[] args)
+    {
+        int n;int sum =0;
+        Scanner s = new Scanner (System.in);
+        System.out.println("no.of elements in array");
+        n=s.nextInt();
+        int[] ar = new int[n];
+        System.out.println("enter values");
+        for(int i=0;i<n;i++)
+        {
+
+            ar[i] = s.nextInt();
+        }
+        for(int k=0;k<n;k++)
+        {
+            sum = sum+ar[k];
+        }
+        System.out.println(sum);
+    }
+}
