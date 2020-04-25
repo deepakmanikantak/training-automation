8d42d0ee963679785947611da6e1375d8b03f7fb
Geetanjali-tachyon <geetanjali.g@tachyontech.com>
2020-04-25 22:01:08 +0530

    Formatted the SimpleArraySum file

--- src/com/trainee/geetanjali/java/SimpleArraySum.java
+++ src/com/trainee/geetanjali/java/SimpleArraySum.java
@@ -1,25 +1,25 @@
 package com.trainee.geetanjali.java;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class SimpleArraySum {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int sum = 0;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter size of the array:");
         int n = s.nextInt();
         int a[] = new int[n];
-        try{
-        System.out.println("Enter array elements:");
-        for(int i = 0; i < n; i++)
-        {
-            a[i] = s.nextInt();
-            sum = sum + a[i];
+        try {
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < n; i++) {
+                a[i] = s.nextInt();
+                sum = sum + a[i];
+            }
+            System.out.println("Sum:" + sum);
+        } catch (InputMismatchException IME) {
+            System.out.println("Enter valid input elements");
         }
-        System.out.println("Sum:"+sum);
-        }
-        catch(InputMismatchException IME){
-            System.out.println("Enter valid input elements");}
     }
 }
 
