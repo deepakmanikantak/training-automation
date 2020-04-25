2c38363a34995bf7d01068ce43062a18bf1c9bb7
phanitachyon <phanindra.k@tachyontech.com>
2020-04-25 22:57:45 +0530

    sum of array

--- src/com/trainee/phani/PhaniTestClass.java
+++ src/com/trainee/phani/PhaniTestClass.java
@@ -2,21 +2,26 @@ package com.trainee.phani;
 import java.util.Scanner;
 public class PhaniTestClass {
 
-        public static void main(String []args){
-            Scanner sc=new Scanner(System.in);
-            int sum=0;
+        public static void main(String []args) {
+            Scanner sc = new Scanner(System.in);
+            int sum = 0;
             System.out.println("enter n value");
-            int n=sc.nextInt();
-            int array[]=new int[n];
-            System.out.println("enter the values");
-            for(int i=0;i<n;i++){
-                array[i]=sc.nextInt();
-            }
-            for(int j=0;j<n;j++){
+            try {
+                int n = sc.nextInt();
+                int array[] = new int[n];
+                System.out.println("enter the values");
+                for (int i = 0; i < n; i++) {
+                    array[i] = sc.nextInt();
+                }
+                for (int j = 0; j < n; j++) {
 
-                sum=sum+array[j];
+                    sum = sum + array[j];
+                }
+                System.out.println("Sum of the array is:" + sum);
             }
-            System.out.println("Sum of the array is:"+sum);
+            catch(Exception e){
+                System.out.println("invalid input pls give valid input");
+            }
         }
     }
 
