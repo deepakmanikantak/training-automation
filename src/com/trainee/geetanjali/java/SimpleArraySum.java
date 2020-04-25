36f3449818bf62e228636a40e189b4d708c0c671
Geetanjali-tachyon <geetanjali.g@tachyontech.com>
2020-04-25 20:54:05 +0530

    Formatted the code

--- src/com/trainee/geetanjali/java/SimpleArraySum.java
+++ src/com/trainee/geetanjali/java/SimpleArraySum.java
@@ -1,21 +1,25 @@
 package com.trainee.geetanjali.java;
-import java.util.*;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SimpleArraySum {
-    public static void main(String args[]){
-        int sum=0;
-        Scanner s=new Scanner(System.in);
+    public static void main(String args[]) {
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter size of the array:");
-        int n=s.nextInt();
-        try{
-        System.out.println("Enter array elements:");
-        int a[]=new int[n];
-        for(int i=0;i<a.length;i++){
-            a[i]=s.nextInt();
-            sum=sum+a[i];}
-            System.out.println("Sum of elements:"+sum);}
-        catch(InputMismatchException IME){
-            System.out.println("Enter a valid input");
+        int n = s.nextInt();
+        try {
+            System.out.println("Enter array elements:");
+            int a[] = new int[n];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = s.nextInt();
+                sum = sum + a[i];
             }
+            System.out.println("Sum of elements:" + sum);
+        } catch (InputMismatchException IME) {
+            System.out.println("Enter a valid input");
+        }
 
     }
 
