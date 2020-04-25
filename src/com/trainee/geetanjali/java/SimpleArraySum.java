2deb6b900429474437f3af4a0c48bc865de05b2f
Geetanjali-tachyon <geetanjali.g@tachyontech.com>
2020-04-25 22:38:35 +0530

    Handled the exception

--- src/com/trainee/geetanjali/java/SimpleArraySum.java
+++ src/com/trainee/geetanjali/java/SimpleArraySum.java
@@ -8,9 +8,9 @@ public class SimpleArraySum {
         int sum = 0;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter size of the array:");
-        int n = s.nextInt();
-        int a[] = new int[n];
         try {
+            int n = s.nextInt();
+            int a[] = new int[n];
             System.out.println("Enter array elements:");
             for (int i = 0; i < n; i++) {
                 a[i] = s.nextInt();
