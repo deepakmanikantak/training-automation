7a3706450d6bdd79d1be701a1030f117dee59f78
phanitachyon <phanindra.k@tachyontech.com>
2020-04-26 22:39:46 +0530

    Maxium elements

--- src/com/trainee/phani/Maxiumelement.java
+++ src/com/trainee/phani/Maxiumelement.java
@@ -1,27 +1,29 @@
 package com.trainee.phani;
+
 import java.util.Scanner;
+
 /**
-*program to find a maximum element in an array
+ * program to find a maximum element in an array
  *
- *@ author phani
+ * @ author phani
  **/
 public class Maxiumelement {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter n value");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         System.out.println("enter the elements");
-        int []arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        int max=arr[0];
-        for(int i=1;i<n;i++){
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
             //comparing the elements to find out the maximum value element in an array
-            if(max<arr[i]){
-                max=arr[i];
+            if (max < arr[i]) {
+                max = arr[i];
             }
         }
-         System.out.println("The maximum number of an array: "+max);
+        System.out.println("The maximum number of an array: " + max);
     }
 }
