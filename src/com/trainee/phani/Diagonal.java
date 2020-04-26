1f95126616d538337537b790ecfbd8e0906f5afb
phanitachyon <phanindra.k@tachyontech.com>
2020-04-26 21:15:37 +0530

    diagonal diffrence

--- src/com/trainee/phani/Diagonal.java
+++ src/com/trainee/phani/Diagonal.java
@@ -3,7 +3,7 @@ package com.trainee.phani;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+//program to difference b/w left diagonal and right diagonal
 public class Diagonal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,17 +22,17 @@ public class Diagonal {
             }
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    if (i == j) {
+                    if (i == j) {    //to identify the left diagonal sum
                         primarysum = primarysum + matrix[i][j];
                     }
-                    if ((i + j) == (size - 1))
+                    if ((i + j) == (size - 1)) //to identify the right diagonal sum
                         secoundarysum = secoundarysum + matrix[i][j];
                 }
             }
             System.out.println("Left Diagonal Sum= " + primarysum);
             System.out.println("Right Diagonal Sum= " + secoundarysum);
-            int diffrence = primarysum - secoundarysum;
-            System.out.println("differnce of two diagonals are: " + diffrence);
+            int diffrence = primarysum - secoundarysum;//to get the differnce
+            System.out.println("differnce of two diagonals are: " +Math.abs(diffrence));
         }catch(InputMismatchException IME){
             System.out.println("Please enter valid input");
         }
