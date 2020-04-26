12e299b21239d510175c584552eb73f9f861701c
Aizaz-tachyon <aizaz.k@tachyontech.com>
2020-04-26 20:35:32 +0530

    "Diagnol difference array(Aizaz)"

--- src/com/trainee/aizaz/java/DiagnolDiffernceArray.java
+++ src/com/trainee/aizaz/java/DiagnolDiffernceArray.java
@@ -1,7 +1,11 @@
 package com.trainee.aizaz.java;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
+/*
+ * the purpose of this class is to show the absolute
+ * difference  between primary and secondary diagnol
+ * of a square matrix array.
+ * */
 
 public class DiagnolDiffernceArray {
     public static void main(String[] args) {
@@ -10,33 +14,27 @@ public class DiagnolDiffernceArray {
         try {
             int n = sc.nextInt();
             int[][] arr = new int[n][n];
-            //insertion of the elements:
+            //insertion of array elements:
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr.length; j++) {
                     System.out.println("Enter the array element arr[" + i + "] [" + j + "]:");
                     arr[i][j] = sc.nextInt();
                 }
             }
-
             int primarySum = 0;
             int secondarySum = 0;
-
             //finding the sum of each diagnol elements:
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr.length; j++) {
+                    //logic for the sum of primary diagnol elements
                     if (i == j) {
                         primarySum = primarySum + arr[i][j];
-
                     }
-
+                    //logic for the sum of secondary diagnol elements
                     if ((i + j) == (n - 1)) {
                         secondarySum = secondarySum + arr[i][j];
-
-
                     }
                 }
-
-
             }
             System.out.println("the sum of primary diagnol is =" + primarySum);
             System.out.println("the sum of secondary diagnol is=" + secondarySum);
@@ -45,16 +43,10 @@ public class DiagnolDiffernceArray {
         } catch (InputMismatchException ime) {
             System.out.println("Please!! Enter a Valid Input");
             System.out.println("Only Numbers Allowed");
-
-
         } catch (NegativeArraySizeException nse) {
-
             System.out.println("Please!! Enter a Valid Input");
             System.out.println("Only Postive  Numners Allowed");
-
         }
-
-
     }
 }
 
