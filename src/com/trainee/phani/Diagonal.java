d2c420bf7d48651969da340a3233c8c45da6c0f8
phanitachyon <phanindra.k@tachyontech.com>
2020-04-26 21:55:54 +0530

    Diagonal Difference

--- src/com/trainee/phani/Diagonal.java
+++ src/com/trainee/phani/Diagonal.java
@@ -3,7 +3,9 @@ package com.trainee.phani;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-//program to difference b/w left diagonal and right diagonal
+/**
+program to difference b/w left diagonal and right diagonal of a matrix using scanner
+ **/
 public class Diagonal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,17 +24,21 @@ public class Diagonal {
             }
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    if (i == j) {    //to identify the left diagonal sum
+                    //to identify the left diagonal sum matrix indexes values [0,0][1,1][2,2]
+                    if (i == j) {
                         primarysum = primarysum + matrix[i][j];
                     }
-                    if ((i + j) == (size - 1)) {//to identify the right diagonal sum
+                    //to identify the right diagonal sum for matrix indexes values [0,2][1,1][2,0]
+                    if ((i + j) == (size - 1)) {
                         secoundarysum = secoundarysum + matrix[i][j];
                 }
                 }
             }
             System.out.println("Left Diagonal Sum= " + primarysum);
             System.out.println("Right Diagonal Sum= " + secoundarysum);
-            int diffrence = primarysum - secoundarysum;//to get the differnce
+            //to get the differnce b/w primary diagonal(left) and secoundary diagonal(right)
+            int diffrence = primarysum - secoundarysum;
+            //Math.abs is used to get the absolute difference
             System.out.println("differnce of two diagonals are: " +Math.abs(diffrence));
         }catch(InputMismatchException IME){
             System.out.println("Please enter valid input");
