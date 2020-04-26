7dccd6ffbcc23c1be47e671254756a9611c3e769
phanitachyon <phanindra.k@tachyontech.com>
2020-04-26 22:30:12 +0530

    Duplicate elements of an array

---
+++ src/com/trainee/phani/Duplicate.java
@@ -0,0 +1,28 @@
+package com.trainee.phani;
+import java.util.Scanner;
+/**
+ * prg to find the duplicate elements in an array
+ *
+ * @author phani
+ */
+public class Duplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n value");
+        int n = sc.nextInt();
+        System.out.println("enter the elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < n; j++) {
+                //comparing elemets to find duplicate elements(repeated elments in an array
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate elements are " + arr[j]);
+                }
+            }
+
+        }
+    }
+}
