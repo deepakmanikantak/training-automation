78e24e286b8e43d8cb4013886cb644b043119ce0
Geetanjali-tachyon <geetanjali.g@tachyontech.com>
2020-04-27 11:48:19 +0530

    Modified  by Handling the exception in DeletingGivenElementInArray program

--- src/com/trainee/geetanjali/java/DeletingGivenElementInArray.java
+++ src/com/trainee/geetanjali/java/DeletingGivenElementInArray.java
@@ -1,5 +1,6 @@
 package com.trainee.geetanjali.java;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -11,32 +12,42 @@ public class DeletingGivenElementInArray {
     public static void main(String args[]) {
         int count = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter Array Size : ");
-        int n = scan.nextInt();
-        System.out.print("Enter Element to be Delete : ");
-        int d = scan.nextInt();
-        int arr[] = new int[n];
-        //read the elements from the user
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter arr[" + i + "]=");
-            arr[i] = scan.nextInt();
-        }
-        //this logic is to find the element to be deleted and to delete that element
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == d) {
+        try {
+            System.out.print("Enter Array Size : ");
+            int n = scan.nextInt();
+            System.out.print("Enter Element to be Delete : ");
+            int d = scan.nextInt();
+            int arr[] = new int[n];
+            //read the elements from the user
+            for (int i = 0; i < n; i++) {
+                System.out.print("Enter arr[" + i + "]=");
+                arr[i] = scan.nextInt();
+            }
+            //this logic is to find the element to be deleted and to delete that element
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == d) {
 
-                count++;
-                break;
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.print("Element Not Found..!!");
+            } else {
+                System.out.println("Element Deleted Successfully..!!");
+                System.out.println("Now the New Array is :");
+                for (int i = 0; i < (n - 1); i++) {
+                    System.out.print(arr[i] + " ");
+                }//System.out.println(Arrays.toString(arr));
             }
         }
-        if (count == 0) {
-            System.out.print("Element Not Found..!!");
-        } else {
-            System.out.println("Element Deleted Successfully..!!");
-            System.out.println("Now the New Array is :");
-            for (int i = 0; i < (n - 1); i++) {
-                System.out.print(arr[i] + " ");
-            }//System.out.println(Arrays.toString(arr));
+        //this block catches the exception when we give an input other than integer
+        catch (InputMismatchException IME) {
+            System.out.println("Enter a valid input");
+        }
+        //this block catches the exception when we give array size negative
+        catch (NegativeArraySizeException NASE) {
+            System.out.println("Array Size should not be negative");
         }
     }
 }
