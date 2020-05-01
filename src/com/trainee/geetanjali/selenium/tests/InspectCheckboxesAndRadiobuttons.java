40971814095ef8936778abb492e5da511af24bb6
Geetanjali-tachyon <geetanjali.g@tachyontech.com>
2020-05-01 17:58:23 +0530

    resolved the comments

--- src/com/trainee/geetanjali/selenium/tests/InspectCheckboxesAndRadiobuttons.java
+++ src/com/trainee/geetanjali/selenium/tests/InspectCheckboxesAndRadiobuttons.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- * I have used this class to verify the functionalities of radio buttons and checkboxes and to find number of checkboxes and radio buttons present by taking a demo site
+ * Purpose of this class to verify the functionalities of radio buttons and checkboxes and to find number of checkboxes and radio buttons present by taking a demo site
  *
  * @author geetanjali
  */
@@ -41,24 +41,27 @@ public class InspectCheckboxesAndRadiobuttons {
         radio2click.click();
         //verifying whether the unselected radio button is selected or not and slected button is unselected or not
         if (radio1click.isSelected()) {
-            System.out.println("radio button is selected");
+            System.out.println("radio1 button is selected : Fail");
         } else {
-            System.out.println("radio button is not selected");
+            System.out.println("radio1 button is not selected: Pass");
         }
         //checking whether we can check and uncheck the checkboxes
         WebElement checkbox1 = driver.findElement(By.id("vfb-6-0"));
         checkbox1.click();
         //checking for the condition whether checkbox is selected or not
-        if (checkbox1.isSelected() == true) {
-            System.out.println("checked the checkbox1");
+        if (checkbox1.isSelected()) {
+            System.out.println("checked the checkbox1: Pass");
+        } else {
+            System.out.println("Not checked the checkbox1:Fail");
         }
         checkbox1.click();
         //checking for the codition whether the checkox is unchecked or not
-        if (checkbox1.isSelected() == false) {
-            System.out.println("unchecked the checkbox1");
-
+        if (checkbox1.isSelected()) {
+            System.out.println("Notunchecked the checkbox1:Fail");
+        } else {
+            System.out.println("Unchecked the checkbox1:Pass");
         }
-
+        driver.quit();
     }
 }
 
