66636c9583b56a5a1fc748a7a665aa05bb89264d
Aizaz-tachyon <aizaz.k@tachyontech.com>
2020-05-08 14:25:09 +0530

    "Deleting"

---
+++ src/com/trainee/aizaz/java/ConvertingStringToUpperCase.java
@@ -0,0 +1,18 @@
+package com.trainee.aizaz.java;
+import java.util.Scanner;
+
+/**
+ * The purpose of this class is to convert string into uppercase
+ */
+public class ConvertingStringToUpperCase {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str=sc.nextLine();
+        /**
+         * toUpperCase() is the method used to convert string into uppercase.
+         */
+        String str2=str.toUpperCase();
+        System.out.println(str2);
+    }
+}
